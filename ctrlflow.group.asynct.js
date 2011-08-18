fe196d8996663fcf8b041a83f46027cc52d79c30
Dominic Tarr <dominic.tarr@gmail.com>
2011-08-18 11:28:36 +1000

    remove tests for old group API

--- ctrlflow.group.asynct.js
+++ ctrlflow.group.asynct.js
@@ -1,107 +1,6 @@
 
 var ctrl = require('ctrlflow')
   , it = require('it-is')
-  , curry = require('curry')
-
-exports.group = function (test){
-  var g = ctrl.group(function (err,results){
-    it(err).equal(null)
-    it(results).property('length',3)
-    test.done()
-  })
-  
-  setTimeout(g(),100)
-  setTimeout(g(),50)
-  setTimeout(g(),150)
-  
-}
-
-exports['group with error'] = function (test){
-  var error = new Error("ERR")
-    , g = ctrl.group(function (err,results){
-    it(err).equal(error)
-    it(results).property('length',3)
-    test.done()
-  })
-
-  setTimeout(g(),100)
-  f = g()
-  setTimeout(function () {
-    f(error)
-  },50)
-  setTimeout(g(),150)
-}
-
-exports['group with args'] = function (test){
-  var error = new Error("ERR")
-    , g = ctrl.group(function (err,results){
-    it(err).equal(error)
-    it(results).property('length',3)
-    it(results).has([
-      [null,1,2,3]
-    , [it.equal(error),"message!"]
-    , [null,{obj: 54}]
-    ])
-    test.done()
-  })
-
-  setTimeout(curry(g(),[null,1,2,3]),100)
-  setTimeout(curry(g(),[error,"message!"]),50)
-  setTimeout(curry(g(),[null,{obj: 54}]),150)
-}
-
-
-exports.group2 = function (test){
-  var g = ctrl.group()
-  
-  setTimeout(g(),100)
-  setTimeout(g(),50)
-  setTimeout(g(),150)
-  
-  g.done(function (err,results){
-    it(err).equal(null)
-    it(results).property('length',3)
-    test.done()
-  })
-}
-
-exports['group with error 2'] = function (test){
-  var error = new Error("ERR")
-    , g = ctrl.group()
-
-  setTimeout(g(),100)
-  f = g()
-  setTimeout(function () {
-    f(error)
-  },50)
-  setTimeout(g(),150)
-  
-  g.done(function (err,results){
-    it(err).equal(error)
-    it(results).property('length',3)
-    test.done()
-  })
-}
-
-exports['group with args'] = function (test){
-  var error = new Error("ERR")
-    , g = ctrl.group()
-  
-  setTimeout(curry(g(),[null,1,2,3]),100)
-  setTimeout(curry(g(),[error,"message!"]),50)
-  setTimeout(curry(g(),[null,{obj: 54}]),150)
-  
-  g.done(function (err,results){
-    it(err).equal(error)
-    it(results).property('length',3)
-    it(results).has([
-      [null,1,2,3]
-    , [it.equal(error),"message!"]
-    , [null,{obj: 54}]
-    ])
-    test.done()
-  })
-}
 
 exports.group2 = function (test){
   var called = 0
@@ -195,4 +94,32 @@ exports['seq can take parallel group'] = function (test) {
     it(called).equal(11)
     test.done()
   })
+}
+
+exports['group can be seq array - edgecase'] = function (test){
+  var called = 0
+    , count = 0
+    
+  function f (add, cb) {
+    called ++
+    count += add
+    this.next()
+  }
+  var g = ctrl.group({
+    a: f,
+    b: [f,f,f],
+    c: [[f, 10],[f, 20]],
+    /*d: {
+      A: f,
+      B: [[f, 30], f]
+    }*/
+  }, function (err,results){
+    if(err) throw err
+    it(called).equal(6)
+    it(count).equal(30)
+    it(results).has({
+      a: []
+    })
+    test.done()
+  })  
 }
